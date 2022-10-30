7533eba6db855a385b1d103d868cda8bf5424c33
HCA01553 <116924963+HCA01553@users.noreply.github.com>
2022-10-30 19:49:10 +0900

    MakeCodeプロジェクトの初期ファイル

---
+++ main.ts
@@ -0,0 +1,14 @@
+LCD1IN8.LCD_Init()
+LCD1IN8.LCD_Clear()
+LCD1IN8.DrawCircle(
+70,
+67,
+36,
+0,
+DRAW_FILL.DRAW_EMPTY,
+DOT_PIXEL.DOT_PIXEL_1
+)
+LCD1IN8.LCD_Display()
+basic.forever(function () {
+	
+})
