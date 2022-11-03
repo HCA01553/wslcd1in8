52849a10181db6a9ca9f666cb4eb4b717379a7eb
HCA01553 <116924963+HCA01553@users.noreply.github.com>
2022-11-03 15:31:38 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,22 +1,24 @@
 let X2point = 0
 let Xpoint = 0
-let Ypont = 0
+let M = 0
 LCD1IN8.LCD_Init()
 LCD1IN8.LCD_Clear()
 let Y2point = 0
+let Ypont = 0
 basic.forever(function () {
-    basic.showString("" + (input.lightLevel()))
-    Y2point += 4
-    Ypont = Y2point
-    Xpoint = 128 - Math.round(input.lightLevel() / 2)
-    X2point = 128
+    M = input.lightLevel()
+    basic.showString("" + (M))
+    Ypont = Ypont + 3
+    Y2point = Y2point + 3
+    Xpoint = 128
+    X2point = 128 - Math.round(M / 2)
     LCD1IN8.DrawLine(
-    Xpoint,
     Ypont,
-    X2point,
+    Xpoint,
     Y2point,
-    20083,
-    DOT_PIXEL.DOT_PIXEL_4,
+    X2point,
+    56550,
+    DOT_PIXEL.DOT_PIXEL_3,
     LINE_STYLE.LINE_SOLID
     )
     LCD1IN8.LCD_Display()
