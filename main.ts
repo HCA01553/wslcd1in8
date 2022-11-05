5660874ac0a0286636b6824930fd3c6d8b36e2a5
HCA01553 <116924963+HCA01553@users.noreply.github.com>
2022-11-05 16:50:18 +0900

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,25 +1,33 @@
-let X2point = 0
-let Xpoint = 0
 let M = 0
 LCD1IN8.LCD_Init()
 LCD1IN8.LCD_Clear()
 let Y2point = 0
 let Ypont = 0
 basic.forever(function () {
-    M = input.lightLevel()
+    M = input.compassHeading()
     basic.showString("" + (M))
-    Ypont = 128
-    Y2point = 128 - Math.round(M / 2)
-    Xpoint = Xpoint + 3
-    X2point = X2point + 3
-    LCD1IN8.DrawLine(
-    Xpoint,
-    Ypont,
-    X2point,
-    Y2point,
-    56550,
-    DOT_PIXEL.DOT_PIXEL_3,
-    LINE_STYLE.LINE_SOLID
+    LCD1IN8.DrawCircle(
+    80,
+    60,
+    50,
+    0,
+    DRAW_FILL.DRAW_EMPTY,
+    DOT_PIXEL.DOT_PIXEL_1
+    )
+    LCD1IN8.DrawPoint(
+    80,
+    60,
+    0,
+    DOT_PIXEL.DOT_PIXEL_1
     )
     LCD1IN8.LCD_Display()
+    LCD1IN8.DrawLine(
+    80,
+    60,
+    119,
+    78,
+    0,
+    DOT_PIXEL.DOT_PIXEL_1,
+    LINE_STYLE.LINE_SOLID
+    )
 })
