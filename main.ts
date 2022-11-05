2ed7f29ae17f5292cae4fdb8684ed55f5d8e1843
HCA01553 <116924963+HCA01553@users.noreply.github.com>
2022-11-05 18:10:15 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,32 +2,43 @@ let M = 0
 LCD1IN8.LCD_Init()
 LCD1IN8.LCD_Clear()
 let Y2point = 0
-let Ypont = 0
+let Ypoint = 0
+let Xpoint = 80
+Ypoint = 60
+let r = 50
 basic.forever(function () {
     M = input.compassHeading()
+    M = 180
     basic.showString("" + (M))
     LCD1IN8.DrawCircle(
-    80,
-    60,
-    50,
+    Xpoint,
+    Ypoint,
+    r,
     0,
     DRAW_FILL.DRAW_EMPTY,
     DOT_PIXEL.DOT_PIXEL_1
     )
     LCD1IN8.DrawPoint(
-    80,
-    60,
+    Xpoint,
+    Ypoint,
     0,
-    DOT_PIXEL.DOT_PIXEL_1
+    DOT_PIXEL.DOT_PIXEL_3
+    )
+    LCD1IN8.DrawPoint(
+    Xpoint + r * Math.cos(M),
+    Ypoint + r * Math.sin(M),
+    0,
+    DOT_PIXEL.DOT_PIXEL_4
     )
     LCD1IN8.LCD_Display()
     LCD1IN8.DrawLine(
-    80,
-    60,
-    119,
-    78,
+    Xpoint,
+    Ypoint,
+    Xpoint + r * Math.cos(M),
+    Ypoint + r * Math.sin(M),
     0,
     DOT_PIXEL.DOT_PIXEL_1,
     LINE_STYLE.LINE_SOLID
     )
+    LCD1IN8.LCD_Display()
 })
